451598bf90cf2ea6eced73e6035215d69b80dc12
Aulas angular <pedromogly@gmail.com>
2023-06-21 13:28:53 -0300

    criando lógica do login e senha

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,20 +9,40 @@ class Login extends Component {
         this.state = {
             teste: true,
             auth: false,
-            fadeOut: false
-          } 
+            fadeOut: false,
+            logins: {
+                username: '',
+                password: ''
+            }
+          }
+          
+        this.dadosLogin = this.dadosLogin.bind(this);
     }
+
+    dadosLogin(e){
+        //propriedade que enquanto digita nos campos, o valor das state ali em cima está assumindo o valor
+        //do que voce está digitando
+        let alterar = this.state.logins;
+        alterar[e.target.name] = e.target.value;
+        this.setState({logins: alterar})
+    }
+
 
     handleLogin = () => {
             // Lógica de autenticação aqui
-
-
-            // Ativa o fade-out
-            this.setState({ fadeOut: true });
-            
-            setTimeout(() => {
-                this.setState({ auth: true });
-            }, 1000);
+            const { username, password} = this.state.logins;
+            if ((username === 'pedromogly' && password === 'macaco') || 
+                (username === 'teste' && password === 'macaco')
+                ) {
+                // Ativa o fade-out
+                this.setState({ fadeOut: true });
+                
+                setTimeout(() => {
+                    this.setState({ auth: true });
+                }, 1000);
+            } else {
+                alert("LOGIN OU SENHA INCORRETA")
+            }
     }
 
     render() { 
@@ -45,7 +65,8 @@ class Login extends Component {
                                         <path fill="#B1B7C4" d="M8.9,7.2C9,6.9,9,6.7,9,6.5v-4C9,1.1,7.9,0,6.5,0h-1C4.1,0,3,1.1,3,2.5v4c0,0.2,0,0.4,0.1,0.7 C1.3,7.8,0,9.5,0,11.5V13h12v-1.5C12,9.5,10.7,7.8,8.9,7.2z M4,2.5C4,1.7,4.7,1,5.5,1h1C7.3,1,8,1.7,8,2.5v4c0,0.2,0,0.4-0.1,0.6 l0.1,0L7.9,7.3C7.6,7.8,7.1,8.2,6.5,8.2h-1c-0.6,0-1.1-0.4-1.4-0.9L4.1,7.1l0.1,0C4,6.9,4,6.7,4,6.5V2.5z M11,12H1v-0.5 c0-1.6,1-2.9,2.4-3.4c0.5,0.7,1.2,1.1,2.1,1.1h1c0.8,0,1.6-0.4,2.1-1.1C10,8.5,11,9.9,11,11.5V12z"/>
                                     </svg>
                                     </label>
-                                    <input id="username" className='lf--input' placeholder='Username' type='text'></input>
+                                    <input id="username" className='lf--input' placeholder='Username' type='username' name="username"
+                                    value={this.state.logins.username} onChange={this.dadosLogin}></input>
                                 </div>
                                 <div className="flex-row">
                                     <label className="lf--label" for="password">
@@ -55,7 +76,8 @@ class Login extends Component {
                                         </g>
                                     </svg>
                                     </label>
-                                    <input id="password" className="lf--input" placeholder='Password' type='password'></input>
+                                    <input id="password" className="lf--input" placeholder='Password' type='password' name="password"
+                                    value={this.state.logins.password} onChange={this.dadosLogin}></input>
                                 </div>
                                 <button className="lf--submit" type='button' value='LOGIN' onClick={this.handleLogin}>LOGIN</button>
                             </form> 
